d3e41dd65cefdd41e5610a489c672d546920761d
onochanel56-prog <onochanel56@gmail.com>
2025-11-21 17:46:38 +0700

    save file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+class Config{
+    private const DBHOST = "localhost ";
+    private const DBUSER = "root";
+    private const DBPASS = "root";
+    private const DBNAME = "restful";
+
+    // 
+}
+?>
