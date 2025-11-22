9d4b6a0c1181c4ba979fd3286b4ae0982cef0d35
onochanel56-prog <onochanel56@gmail.com>
2025-11-22 10:43:06 +0700

    ข้อความบันทึกงาน

--- config.php
+++ config.php
@@ -2,8 +2,36 @@
 class Config{
     private const DBHOST = "localhost ";
     private const DBUSER = "root";
-    private const DBPASS = "root";
+    private const DBPASS = "";
     private const DBNAME = "restful";
+    // data source name
+
+    private $dsn ="mysql:host=" . self:: DBHOST. ";dbname=" . self ::DBNAME . ""
+    private $conn = null;
+
+
+    public function __construct()
+    {
+        try {
+            $this->conn = new PDO($this->dsn, self::DBUSER, self::DBPASS);
+            $this->conn->setAttrbute(PDO::ATT_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        }catch (PDOException $e){
+            die("Connect failed: ". $e->getMessage());
+        }
+        return $this->conn;
+    }
+
+    public function test_input($data){
+        $data = strip_tags($data){
+            $data = htmlspecialchars ($data);
+            $data = stripcslashes ($data);
+            $data = trim ($data);
+            return $data;
+        }
+        public function message($content, $status){
+            return json_encode(['message '=> $content , 'error'=> $status])
+        }
+    }
 
     // 
 }
